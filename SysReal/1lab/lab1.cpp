6f074c8e6f88ff1892fef30c35ab116c85c5c618
Даня Ходосевич <114701605+SpaghettSauce@users.noreply.github.com>
2024-10-24 00:08:22 +0200

    л

--- SysReal/1lab/lab1.cpp
+++ SysReal/1lab/lab1.cpp
@@ -1,13 +1,29 @@
 #include <iostream>
 using namespace std;
-int main(){
 
+int factorial(){
+    int sum = 1;
+    for (int j = 2; j <= 10; j++){
+        sum *= j;
+    }
+    return sum;
+}
+
+
+int main(){
+    //1
     long s = 1.0;
     for(int i = 1; i <33; i++ )
     {
     s *= i;
     }
 
-    cout<<s;
-    return 0;
+    //cout<<s;
+    
+    //2
+    for (int j = 0; j < 10000000; j++){
+        factorial();
+    }
+
+
 }

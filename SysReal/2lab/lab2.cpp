5c526c7ec2964ed30ad7311a494184c04933c406
Даня Ходосевич <114701605+SpaghettSauce@users.noreply.github.com>
2024-11-19 00:54:21 +0200

    lab 2

--- SysReal/2lab/lab2.cpp
+++ SysReal/2lab/lab2.cpp
@@ -1,19 +1,36 @@
+#include <iostream>
 #include <thread>
 #include <mutex>
+#include <chrono>
 #include <string>
+#include <cmath>
 
-void Func(string name)
-{
-   
+std::mutex m; 
+
+void Func(std::string name) {
+    long double i = 0.0;
+    auto start_time = std::chrono::high_resolution_clock::now();
+    auto end_time = start_time + std::chrono::seconds(1);
+
+    while (std::chrono::high_resolution_clock::now() < end_time) {
+        i += pow(10,-9); 
+    }
+
+  
+    m.lock();
+    std::cout << name << ": " << i << std::endl;
+    m.unlock();
 }
 
-int main()
-{
+int main() {
     std::thread thread1(Func, "t1");
     std::thread thread2(Func, "t2");
     std::thread thread3(Func, "t3");
+
     thread1.join();
     thread2.join();
     thread3.join();
-    system("pause");
+
+    system("pause"); 
+    return 0;
 }
